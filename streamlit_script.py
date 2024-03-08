723c2c77e46a036498050459b602dc03e115407c
jasondetaylor <jasondetaylor@gmail.com>
2024-03-07 16:46:41 -0800

    reduced scope to simple checkboxes. added cache to random selection function to aviod refreshing options when selected

--- streamlit_script.py
+++ streamlit_script.py
@@ -3,10 +3,12 @@ import streamlit as st
 import pandas as pd
 
 # import dataframes from jupyter notebook
+# should add cache here as only need to import once
 df = pd.read_csv('df.csv', header = [0, 1], index_col = 0) # our cleaned data
 user_input_vars = pd.read_csv('user_input_vars.csv') # our user input variables
 
 # function to select random options for a given source
+@st.cache_data()
 def select_random_options(df, n):
     '''takes in a dataframe and specified number of options, returns a dictionary containing lists of n number of metrics. 
     1 dict entry per metric group as governed by source col'''
@@ -24,7 +26,7 @@ user_options = select_random_options(user_input_vars, n = n)
 st.title('Select options that rsonate with you from the list below:')
 
 # display user input options under separate columns
-col_list = st.beta_columns(len(user_options))  # dplit the screen into columns
+col_list = st.columns(len(user_options))  # dplit the screen into columns
 selected_options = {}  # to store selected options
 
 for idx, (source, options) in enumerate(user_options.items()):
@@ -33,11 +35,3 @@ for idx, (source, options) in enumerate(user_options.items()):
         selected_options[source] = []
         for option in options:
             selected = st.checkbox(option)
-            if selected:
-                selected_options[source].append(option)
-
-# display selected options
-st.write("Selected Options:")
-for source, options in selected_options.items():
-    for option in options:
-        st.write(f'You selected {option} from {source.capitalize()}')
