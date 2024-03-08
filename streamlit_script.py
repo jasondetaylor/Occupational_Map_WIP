767632033caa20fc4c8add96fe3d34af2aaa6e7e
jasondetaylor <jasondetaylor@gmail.com>
2024-03-07 16:19:11 -0800

    read in all data, created checkbox options for user to select metrics as basis for user input vector. need to address refreshing issue

--- streamlit_script.py
+++ streamlit_script.py
@@ -2,6 +2,42 @@
 import streamlit as st
 import pandas as pd
 
+# import dataframes from jupyter notebook
+df = pd.read_csv('df.csv', header = [0, 1], index_col = 0) # our cleaned data
+user_input_vars = pd.read_csv('user_input_vars.csv') # our user input variables
 
-df = pd.read_csv('df.csv', header = [0, 1, 2], index_col = 0)
-st.dataframe(df)
+# function to select random options for a given source
+def select_random_options(df, n):
+    '''takes in a dataframe and specified number of options, returns a dictionary containing lists of n number of metrics. 
+    1 dict entry per metric group as governed by source col'''
+    
+    variable_lists = {}
+    for var in df['Source'].unique():
+        variable_lists[var] = df[df['Source'] == var].sample(n)['Element Name'].tolist()
+    return variable_lists
+
+# select 10 random options for each category
+n = 10
+user_options = select_random_options(user_input_vars, n = n)
+
+# STREAMLIT APP
+st.title('Select options that rsonate with you from the list below:')
+
+# display user input options under separate columns
+col_list = st.beta_columns(len(user_options))  # dplit the screen into columns
+selected_options = {}  # to store selected options
+
+for idx, (source, options) in enumerate(user_options.items()):
+    with col_list[idx]:
+        st.subheader(f'{source.capitalize()}:')
+        selected_options[source] = []
+        for option in options:
+            selected = st.checkbox(option)
+            if selected:
+                selected_options[source].append(option)
+
+# display selected options
+st.write("Selected Options:")
+for source, options in selected_options.items():
+    for option in options:
+        st.write(f'You selected {option} from {source.capitalize()}')
